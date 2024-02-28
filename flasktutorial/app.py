11a153b77dfc8ca96c5566d339876c86fbe828b2
BYUNG CHAN WOO <byungchanwoo@BYUNGui-iMac.local>
2024-02-28 22:42:54 +0900

    new feature added(file select by combobox)

--- flasktutorial/app.py
+++ flasktutorial/app.py
@@ -6,7 +6,26 @@ import uuid
 
 app = Flask(__name__)
 
-data_file_path = 'data.txt'
+data_file_path = 'intro.txt'
+data_folder = 'data'
+
+
+
+def get_text_files():
+    text_files = []
+    for file in os.listdir(data_folder):
+        if file.endswith('.txt'):
+            text_files.append(file)
+    return text_files
+
+def update_file_path(new_path):
+    global data_file_path
+    data_file_path = new_path
+
+def read_file(file_name):
+    file_path = os.path.join(data_folder, file_name)
+    with open(file_path, 'r') as file:
+        return file.read()
 
 def save_data(post_id, data):
     with open(data_file_path, 'a') as file:
@@ -15,9 +34,11 @@ def save_data(post_id, data):
 def load_data():
     try:
         with open(data_file_path, 'r') as file:
+            print("sucess open ", data_file_path)
             lines = file.readlines()
             return [line.strip().split(',', 1) for line in lines]
     except FileNotFoundError:
+        print("load data not found")
         return []
 
 def update_data(post_id, new_data):
@@ -53,47 +74,68 @@ def add_data(post_id, amount):
 @app.route('/')
 def index():
     posts = load_data()
-    return render_template('index.html', posts=posts)
+    text_files = get_text_files()
+    return render_template('index.html', posts=posts,text_files=text_files)
 
 @app.route('/process', methods=['POST'])
 def process():
     user_input = request.form['user_input']
-    post_id = str(uuid.uuid4())
-    save_data(post_id, user_input)
+    if user_input != "":
+        print("add", user_input, ",")
+        post_id = str(uuid.uuid4())
+        save_data(post_id, user_input)
+
+    selected_file=request.form['selected_file']
+    print("selcted_file", selected_file)
+    if selected_file != "nonselect":
+        update_file_path(os.path.join(data_folder, selected_file))
+
+
+    print(data_file_path)
+
+            
     posts = load_data()
-    return render_template('index.html', posts=posts)
+    text_files = get_text_files()
+    return render_template('index.html', posts=posts, text_files=text_files) #,selected_file=selected_file)
 
 @app.route('/update/<post_id>', methods=['POST'])
 def update(post_id):
     new_data = request.form['modified_data']
     update_data(post_id, new_data)
     posts = load_data()
-    return render_template('index.html', posts=posts)
+    text_files = get_text_files()
+    return render_template('index.html', posts=posts, text_files=text_files)
 
 @app.route('/modify/<post_id>', methods=['GET'])
 def modify(post_id):
     posts = load_data()
     selected_post = [data for data in posts if data[0] == post_id][0]
-    return render_template('modify.html', post_id=post_id, post_content=selected_post[1])
+    text_files = get_text_files()
+    return render_template('modify.html', post_id=post_id, post_content=selected_post[1], text_files=text_files)
+
 
 @app.route('/delete/<post_id>')
 def delete(post_id):
     delete_data(post_id)
     posts = load_data()
-    return render_template('index.html', posts=posts)
+    text_files = get_text_files()
+    return render_template('index.html', posts=posts, text_files=text_files)
 
 @app.route('/up/<post_id>')
 def up(post_id):
     add_data(post_id,1)
     posts = load_data()
-    return render_template('index.html', posts=posts)
+    text_files = get_text_files()
+    return render_template('index.html', posts=posts, text_files=text_files)
 
 @app.route('/down/<post_id>')
 def down(post_id):
     add_data(post_id,-1)
     posts = load_data()
-    return render_template('index.html', posts=posts)
+    text_files = get_text_files()
+    return render_template('index.html', posts=posts,text_files=text_files)
 
 
 if __name__ == '__main__':
+    update_file_path(os.path.join(data_folder, data_file_path))
     app.run(debug=True)
